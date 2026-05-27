1379c0478f5f7f0a5d4dd99927c21f1f43933d59
alekseynikonorovgeo-spec <alekseynikonorovgeo@gmail.com>
2026-05-27 15:32:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // При обновлении HTML или иконок — увеличивайте номер версии (v4 → v5 → v6 ...)
 // чтобы новая версия гарантированно скачалась у всех прорабов.
 
-const CACHE_NAME = 'ochakovo-v13';
+const CACHE_NAME = 'ochakovo-v15';
 const FILES_TO_CACHE = [
   './',
   './index.html',
